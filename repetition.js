b30ade3f80aacf6fb57d05ac7c916cf06d911293
juliaprytz <juliaprytznasam12@gmail.com>
2016-05-02 13:15:17 +0200

    Update

--- repetition.js
+++ repetition.js
@@ -1,1 +1,12 @@
 //Javascript
+
+//Array
+//En första Array (metod 1)
+//Exempel 1
+var weekdays = ["Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag", "Söndag"];
+
+//Hämta ur Array..
+console.log("Exempel 1. Första veckodagen " + weekday[0]);
+
+var favoriteDay = weekday[5];
+
