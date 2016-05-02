987e8d1b8bab7b3aec4023ff9fe4d72ae790b390
juliaprytz <juliaprytznasam12@gmail.com>
2016-05-02 13:22:26 +0200

    Month

--- repetition.js
+++ repetition.js
@@ -8,4 +8,18 @@ var weekdays = ["Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag", "
 //Hämta ur Array..
 console.log("Exempel 1. Första veckodagen " + weekdays[0]);
 
+var month = [];
+month[0] = "Januari";
+month[1] = "Feb";
+month[2] = "Mars";
+month[3] = "April";
+month[4] = "Maj";
+month[5] = "Juni";
+month[6] = "Juli";
+month[7] = "Augusti";
+month[8] = "September";
+month[9] = "Okt";
+month[10] = "Nov";
+month[11] = "Dec";
+
 
